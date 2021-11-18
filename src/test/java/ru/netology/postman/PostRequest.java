94158f74382c381ea14e57c3451a116107717f3f
Viktoria <vikitoga@gmail.com>
2021-11-18 16:32:44 +0300

    project created

---
+++ src/test/java/ru/netology/postman/PostRequest.java
@@ -0,0 +1,26 @@
+package ru.netology.postman;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostRequest {
+
+    @Test
+    void shouldPostData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+
+    }
+}
